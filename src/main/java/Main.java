17d099aa0d27519c4ea7b7ac8b462998b1f7fd40
Hailubby <hwan561@aucklanduni.ac.nz>
2017-04-20 11:06:02 +1200

    Code for java parser to print class and visit methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,41 @@
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+
 /**
  * Created by User on 18/04/2017.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, URISyntaxException {
         System.out.println("Hello frands");
+
+        File sourceFile = new File(Main.class.getClass().getResource("/AppJavaSrc/com/jjhhh/dice/CustomDiceActivity.java").toURI());
+
+        //parse the file
+        CompilationUnit compilationUnit = JavaParser.parse(sourceFile);
+
+        // prints the resulting compilation unit to default system output
+        //System.out.println(compilationUnit.toString());
+
+        // visit and print the methods names
+        new MethodVisitor().visit(compilationUnit, null);
+    }
+    /**
+     * Simple visitor implementation for visiting MethodDeclaration nodes.
+     */
+    private static class MethodVisitor extends VoidVisitorAdapter<Void> {
+        @Override
+        public void visit(MethodDeclaration n, Void arg) {
+            /* here you can access the attributes of the method.
+             this method will be called for all methods in this
+             CompilationUnit, including inner class methods */
+            System.out.println(n.getName());
+            super.visit(n, arg);
+        }
     }
 }
